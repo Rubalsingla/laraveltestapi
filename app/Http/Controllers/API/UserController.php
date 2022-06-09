b06a7f969eaedcb8cf0d540a8b65cf65d15a1bed
Attu123 <rubalsingla19@gmail.com>
2022-06-09 16:56:46 +0530

    Your message test

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,8 +14,13 @@ public $successStatus = 200;
      * @return \Illuminate\Http\Response 
      */ 
     public function login(){ 
+
+        echo"hhhh";
+
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
+
+
             $success['token'] =  $user->createToken('MyApp')-> accessToken; 
             return response()->json(['success' => $success], $this-> successStatus); 
         } 
