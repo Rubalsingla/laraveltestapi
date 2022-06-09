330c72ef43267953901a133747e3b26cfa577f17
Rubalsingla <89590405+Rubalsingla@users.noreply.github.com>
2022-06-09 18:07:39 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -15,7 +15,7 @@ public $successStatus = 200;
      */ 
     public function login(){ 
 
-        echo"hhhh";
+        #echo"hhhh";
 
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
